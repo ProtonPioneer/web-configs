376554d9f1b19aa470d6a6fec5f1c4358885f37e
waterwelly <68794849+waterwelly@users.noreply.github.com>
2022-01-24 13:19:37 +0800

    Create main.js

---
+++ jsproxy/main.js
@@ -0,0 +1,17 @@
+function createNode() {
+    var container = document.createDocumentFragment();
+    var e_0 = document.createElement("div");
+    var e_1 = document.createElement("script");
+    e_1.setAttribute("type", "text/javascript");
+    e_1.setAttribute("src", "https://waterwelly.github.io/configs/jsproxy/main_1.js");
+    e_1.setAttribute("async", "async");
+    e_0.appendChild(e_1);
+    var e_2 = document.createElement("script");
+    e_2.setAttribute("type", "text/javascript");
+    e_2.setAttribute("src", "https://waterwelly.github.io/configs/jsproxy/main_2.js");
+    e_2.setAttribute("async", "async");
+    e_0.appendChild(e_2);
+    container.appendChild(e_0);
+    return container;
+}
+document.body.appendChild(createNode())
